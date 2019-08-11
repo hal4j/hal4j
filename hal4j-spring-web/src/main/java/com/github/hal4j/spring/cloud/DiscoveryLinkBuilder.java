ac66c1320e4b14b5fb509537fd0a78e7979379ef
Ivan Gammel <ivan-gammel@users.noreply.github.com>
2019-08-11 22:23:37 +0200

    Integrated Zuul filter with discovery client

--- hal4j-spring-web/src/main/java/com/github/hal4j/spring/cloud/DiscoveryLinkBuilder.java
+++ hal4j-spring-web/src/main/java/com/github/hal4j/spring/cloud/DiscoveryLinkBuilder.java
@@ -4,17 +4,11 @@ import com.github.hal4j.spring.HypermediaRequest;
 import com.github.hal4j.spring.web.SpringWebLinkBuilder;
 import com.github.hal4j.uritemplate.URIBuilder;
 
-import java.util.Set;
-import java.util.stream.Stream;
-
 import static com.github.hal4j.resources.cloud.DiscoveryClientLinkResolver.SERVICE_NS;
 import static com.github.hal4j.uritemplate.URIBuilder.uri;
-import static com.github.hal4j.uritemplate.URITemplateVariable.template;
-import static java.util.stream.Collectors.toSet;
+import static com.github.hal4j.uritemplate.URITemplateVariable.preEncoded;
 
 public class DiscoveryLinkBuilder extends SpringWebLinkBuilder {
-
-    private static final Set<String> LOCALHOSTS = Stream.of("localhost", "127.0.0.1").collect(toSet());
 
     private final String service;
 
@@ -31,8 +25,6 @@ public class DiscoveryLinkBuilder extends SpringWebLinkBuilder {
     protected URIBuilder link(HypermediaRequest request) {
         String host = request.resolved().host();
         URIBuilder builder = uri(request.resolved().scheme(), host, request.resolved().port());
-        return LOCALHOSTS.contains(host)
-                ? builder
-                : builder.server(template(SERVICE_NS + "." + this.service));
+        return builder.server(preEncoded(SERVICE_NS + "." + this.service));
     }
 }
--- hal4j-spring-zuul/src/main/java/com/github/hal4j/spring/cloud/gw/ResourceResponseFilter.java
+++ hal4j-spring-zuul/src/main/java/com/github/hal4j/spring/cloud/gw/ResourceResponseFilter.java
@@ -2,42 +2,65 @@ package com.github.hal4j.spring.cloud.gw;
 
 import com.github.hal4j.jackson.JacksonHALMapper;
 import com.github.hal4j.resources.*;
+import com.netflix.util.Pair;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 import java.util.function.Function;
 
+import static com.github.hal4j.resources.cloud.DiscoveryClientLinkResolver.SERVICE_NS;
+import static com.github.hal4j.uritemplate.ParamHolder.prefixed;
+import static com.github.hal4j.uritemplate.URIBuilder.uri;
+import static com.github.hal4j.uritemplate.URIFactory.templateUri;
 import static com.netflix.zuul.context.RequestContext.getCurrentContext;
+import static java.util.stream.Collectors.toMap;
 
-public class ResourceResponseFilter extends ZuulFilter implements MergeStrategy {
+public class ResourceResponseFilter extends ZuulFilter {
 
+    private String serviceId;
     private final ResourceFactory factory;
     private final JacksonHALMapper json;
-    private final Function<String, String> mapper;
+    private final DiscoveryClient client;
 
-    public ResourceResponseFilter(ResourceFactory factory,
+    public ResourceResponseFilter(String serviceId,
+                                  ResourceFactory factory,
                                   JacksonHALMapper json,
-                                  Function<String, String> mapper) {
+                                  DiscoveryClient client) {
+        this.serviceId = serviceId;
         this.factory = factory;
         this.json = json;
-        this.mapper = mapper;
+        this.client = client;
     }
 
-    public GenericResource map(GenericResource resource) {
-        return factory.bindGeneric().from(resource, this).asResource();
-    }
+    private static class ResponseMapper implements MergeStrategy {
 
-    @Override
-    public HALLink map(HALLink link) {
-        return HALLinkBuilder.basedOn(link).href(mapper.apply(link.href)).build();
-    }
+        private final ResourceFactory factory;
+        private final Function<String, String> mapping;
 
-    @Override
-    public Object map(Object object, BindingContext context) {
-        return map(context.bind(object, GenericResource.class));
+        ResponseMapper(ResourceFactory factory, Function<String, String> mapping) {
+            this.factory = factory;
+            this.mapping = mapping;
+        }
+
+        @Override
+        public HALLink map(HALLink link) {
+            return HALLinkBuilder.basedOn(link).href(mapping.apply(link.href)).build();
+        }
+
+        @Override
+        public Object map(Object object, BindingContext context) {
+            return map(context.bind(object, GenericResource.class));
+        }
+
+        GenericResource map(GenericResource resource) {
+            return factory.bindGeneric().from(resource, this).asResource();
+        }
     }
 
     @Override
@@ -53,7 +76,10 @@ public class ResourceResponseFilter extends ZuulFilter implements MergeStrategy 
     @Override
     public boolean shouldFilter() {
         RequestContext context = getCurrentContext();
-        String contentType = context.getResponse().getHeader("Content-Type");
+        String contentType = context.getOriginResponseHeaders().stream()
+                .filter(pair -> "Content-Type".equals(pair.first()))
+                .map(Pair::second)
+                .findAny().orElse(null);
         return context.getResponseStatusCode() < 300
                 && contentType != null
                 && contentType.startsWith("application/json");
@@ -64,12 +90,33 @@ public class ResourceResponseFilter extends ZuulFilter implements MergeStrategy 
         try {
             RequestContext context = getCurrentContext();
             InputStream stream = context.getResponseDataStream();
+
             GenericResource response = json.mapper().readValue(stream, GenericResource.class);
-            String mappedString = json.serialize(map(response));
+
+            ResponseMapper responseMapper = createMapper(context);
+            String mappedString = json.serialize(responseMapper.map(response));
+
             context.setResponseBody(mappedString);
         } catch (IOException e) {
             throw new ZuulException(e, 500, "Could not process HAL response");
         }
         return null;
+    }
+
+    private ResponseMapper createMapper(RequestContext context) {
+        HttpServletRequest request = context.getRequest();
+        String scheme = request.getScheme();
+        String host = request.getServerName();
+        int port = request.getServerPort();
+
+        Map<String, String> services = client.getServices().stream()
+                .filter(serviceId::equals)
+                .collect(toMap(
+                        instanceId -> instanceId,
+                        instanceId -> uri(scheme, host, port).relative(instanceId).toString()));
+
+        Function<String, String> mapping = href -> templateUri(href).expand(prefixed(SERVICE_NS, services)).toString();
+
+        return new ResponseMapper(factory, mapping);
     }
 }
